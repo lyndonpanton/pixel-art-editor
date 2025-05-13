ded8016b07f9f0118c5d8df0a60aebf5fce46947
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-13 13:36:01 +0100

    Create 16x16 editor

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", function (e) {
     console.log("DOM content has loaded");
+
+    createEditor(16);
+    
+    function createEditor(sideLength) {
+        let editor = document.getElementById("editor");
+
+        for (let i = 0; i < sideLength; i++) {
+            let editorRow = document.createElement("section");
+            editorRow.classList.add("editor-row");
+
+            for (let j = 0; j < sideLength; j++) {
+                let pixel = document.createElement("article");
+                pixel.classList.add("pixel");
+
+                editorRow.appendChild(pixel);
+            }
+
+            editor.append(editorRow);
+        }
+    }
 });
